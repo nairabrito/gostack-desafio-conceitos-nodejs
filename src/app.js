1ce159f497b36ea1bd55cb55a6f5a2ad370e641a
Naira Brito <naira.sistemas@gmail.com>
2020-08-22 22:56:10 -0300

    Primeiro desafio GoStack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  
+  repositories.push(project);
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const indexRepository = repositories.findIndex
+  (project => 
+    project.id == id
+  );
+
+  if(indexRepository == -1){
+
+    return response.status(400).json({ error: 'Project does not exists.'});
+
+  }
+  const project = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[indexRepository].likes,
+  };
+  repositories[indexRepository] = project;
+  return response.json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const indexRepository = repositories.findIndex 
+  (project => 
+    project.id == id
+  );
+
+  if(indexRepository >= 0){
+    repositories.splice(indexRepository, 1);
+
+  }else {
+    return response.status(400).json({ error: 'Project does not exists.'});
+
+  }
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const indexRepository = repositories.findIndex
+    (project =>
+      project.id == id
+    );
+    if(indexRepository == -1){
+
+      return response.status(400).json({ error: 'Project does not exists.'}); 
+    }
+    repositories[indexRepository].likes += 1;
+
+    return response.json(repositories[indexRepository]);
 });
 
 module.exports = app;
